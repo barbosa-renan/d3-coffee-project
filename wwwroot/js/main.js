11c0bf58113c1bda121656107f3c1effd30cfada
Renan Barbosa <renan.vinicius@sga.pucminas.br>
2020-07-22 22:51:10 -0300

    Implementação do Update Pattern

--- wwwroot/js/main.js
+++ wwwroot/js/main.js
@@ -8,6 +8,8 @@ var margin = { left: 80, right: 20, top: 50, bottom: 100 };
 var width = 600 - margin.left - margin.right;
 var height = 400 - margin.top - margin.bottom;
 
+var flag = true;
+
 var g = d3.select("#coffee-area")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -16,6 +18,21 @@ var g = d3.select("#coffee-area")
     .attr("transform", "translate(" + margin.left + "," +
         margin.top + ")");
 
+var xAxistGroup = g.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")");
+
+var yAxisGroup = g.append("g")
+    .attr("class", "y axis");
+
+var x = d3.scaleBand()
+    .range([0, width])
+    .padding(0.2);
+
+var y = d3.scaleLinear()
+    .range([height, 0]);
+
+// X Label
 g.append("text")
     .attr("class", "x axis-label")
     .attr("x", width / 2)
@@ -24,66 +41,70 @@ g.append("text")
     .attr("text-anchor", "middle")
     .text("Month");
 
-g.append("text")
+// Y Label
+var yLabel = g.append("text")
     .attr("class", "y axis-label")
     .attr("y", -60)
     .attr("x", -(height / 2))
     .attr("font-size", "20px")
     .attr("text-anchor", "middle")
-    .text("Revenue")
-    .attr("transform", "rotate(-90)");
+    .attr("transform", "rotate(-90)")
+    .text("Revenue");
 
 d3.json("data/revenues.json").then(function(data) {
 
     data.forEach(d => {
-
         d.revenue = +d.revenue;
-        console.log(d.revenue);
+        d.profit = +d.profit;
     });
 
-    var x = d3.scaleBand()
-        .domain(data.map(function(d) {
-            return d.month;
-        }))
-        .range([0, width])
-        .padding(0.2);
+    d3.interval(function() {
+        update(data);
+        flag = !flag;
+    }, 1000);
 
-    var y = d3.scaleLinear()
-        .domain([0, d3.max(data, function(d) {
-            return d.revenue;
-        })])
-        .range([height, 0]);
+    // Run de vis for the first time
+    update(data);
+});
 
+function update(data) {
+
+    var value = flag ? "revenue" : "profit";
+
+    x.domain(data.map(function(d) { return d.month; }));
+    y.domain([0, d3.max(data, function(d) { return d[value]; })]);
+
+    // X Axis
     var xAxisCall = d3.axisBottom(x);
-    g.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxisCall);
+    xAxistGroup.call(xAxisCall)
 
+    // Y Axis
     var yAxisCall = d3.axisLeft(y)
         .tickFormat(function(d) { return "$" + d; });
-    g.append("g")
-        .attr("class", "y axis")
-        .call(yAxisCall);
+    yAxisGroup.call(yAxisCall)
 
-    g.append("g")
-        .attr("class", "y-axis")
-        .call(yAxisCall);
-
+    // JOIN new data with old elements    
     var rects = g.selectAll("rect")
         .data(data)
-        .enter()
-        .append("rect")
-        .attr("x", function(d, i) {
-            return x(d.month);
-        })
-        .attr("y", function(d) {
-            return y(d.revenue);
-        })
-        .attr("height", function(d) {
 
-            return height - y(d.revenue);
-        })
+    // EXIT old elements not present in new data.
+    rects.exit().remove();
+
+    // UPDATE old elements present in new data.
+    rects.attr("x", function(d, i) { return x(d.month); })
+        .attr("y", function(d) { return y(d[value]); })
+        .attr("height", function(d) { return height - y(d[value]); })
+        .attr("width", x.bandwidth);
+
+    // ENTER new elements present in new data.
+    rects.enter()
+        .append("rect")
+        .attr("x", function(d, i) { return x(d.month); })
+        .attr("y", function(d) { return y(d[value]); })
+        .attr("height", function(d) { return height - y(d[value]); })
         .attr("width", x.bandwidth)
         .attr("fill", "grey");
-});
+
+    var label = flag ? "Revenue" : "Profit";
+    yLabel.text(label);
+}
